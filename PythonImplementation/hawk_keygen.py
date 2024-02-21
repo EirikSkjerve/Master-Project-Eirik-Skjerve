b887163b635f227248ed2256bf1d4ed7534fda73
Eirik Skjerve <tic013@uib.no>
2024-02-21 10:56:50 +0100

    Checking norm of generated g and f

--- PythonImplementation/hawk_keygen.py
+++ PythonImplementation/hawk_keygen.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import linalg as LA
 #from Crypto.Hash import SHAKE256
 import secrets
 
@@ -18,21 +19,32 @@ def decode_int(bits, k):
 # step 1: sample coefficients of f, g through Bin(n)
 def sample_coefficients(eta, kseed):
     np.random.seed(kseed)
-    centred_samples = np.random.binomial(eta,p=0.5,size=n) - eta/2
+    # numpy's binomial distribution. Stored as np-array of type int
+    centred_samples = np.array(np.random.binomial(eta,p=0.5,size=n) - eta/2, dtype=int)
     
     return centred_samples
 
-def generate_f_g(eta):
+def generate_f_g():
     kseed_f = 13
     f = sample_coefficients(eta, kseed_f)
     kseed_g = 27
     g = sample_coefficients(eta, kseed_g)
 
+    print(f"f: {f}\ng:{g}")
+    verify_f_g(f, g)
     # restart with a new seed if conditions are not fulfilled
     return (f, g) if verify_f_g else generate_f_g(eta)
 
 # step 2: check conditions for f, g
 def verify_f_g(f, g) -> bool:
+    
+    f_norm = LA.norm(f)
+    g_norm = LA.norm(g)
+    
+    # values only for testing
+    if f_norm > 20 or g_norm > 20:
+        print(f"Norm too big!")
+        return False
     return True
 
 # step 3: get r = NTRUSolve(f, g)
@@ -67,5 +79,5 @@ if __name__ == "__main__":
     eta = 2
     # use 'secrets' module for CSPRNG
     #kseed = secrets.randbits(5)
-    kseed = 13
-    sample_coefficients(eta, kseed)
+
+    f, g = generate_f_g()
