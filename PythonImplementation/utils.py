0871dd3a3039f1537913f6469e3e69715bdb53e4
Eirik Skjerve <tic013@uib.no>
2024-03-08 11:32:29 +0100

    Implemented simple polynomial addition, negation and subtraction

--- PythonImplementation/utils.py
+++ PythonImplementation/utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 ### This file contains helper functions for some of the algorithms.
 ### May later be split into several utils-files
 
@@ -12,3 +13,34 @@ def x_gcd(a, b):
     y = x1
 
     return gcd, x, y
+
+# returns -1*f
+def negate_poly(f):
+    return [-f[i] for i in range(len(f))]
+
+# returns sum of two polynomials.
+# optional modulus n
+def poly_add(f, g, n=None):
+    if n:
+        print(f"Not implemented yet")
+        return
+
+    len_f = len(f)
+    len_g = len(g)
+
+    if len_f > len_g:
+        r = np.copy(f)
+        for i in range(len_g):
+            r[i] += g[i]
+        return r
+    if len_f < len_g:
+        r = np.copy(g)
+        for i in range(len_f):
+            r[i] += f[i]
+        return r
+    if len_f==len_g:
+        r = np.array([f[i]+g[i] for i in range(len_f)])
+        return r
+
+def poly_sub(f,g):
+    return poly_add(f, negate_poly(g))
