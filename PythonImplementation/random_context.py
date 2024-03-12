bf4963ffa7546116ef74f1ee57098e298293196c
Eirik.Skjerve <eirik.skjerve@student.uib.no>
2024-03-12 19:20:20 +0100

    Added comment for randomContext class

--- PythonImplementation/random_context.py
+++ PythonImplementation/random_context.py
@@ -3,6 +3,9 @@ import numpy as np
 import random
 
 class RandomContext:
+    '''
+    Generates random numbers in a deterministic way
+    '''
     # initialize context with a base seed
     def __init__(self, base_seed):
         self.base_seed = base_seed
