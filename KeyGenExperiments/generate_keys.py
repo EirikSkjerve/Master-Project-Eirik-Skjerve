b1434acbc0e5f29cce6ea4e029e4ff55fd43a5e5
Eirik.Skjerve <eirik.skjerve@student.uib.no>
2024-04-18 11:47:19 +0200

    Setting seed for several key generations implemented

--- KeyGenExperiments/generate_keys.py
+++ KeyGenExperiments/generate_keys.py
@@ -1,7 +1,9 @@
 # we import HAWK's python implementation of the hawk key generation algorithm
-from hawk_py.keygen import hawkkeygen
+from hawk_py.keygen import hawkkeygen, RngContext
 from hawk_py.ntrugen.ntrugen_hawk import ntru_solve
-def generate_keypairs(num_pairs, logn=8):
+import numpy as np
+
+def generate_keypairs(num_pairs, logn=8, seed=None):
     '''
     Input: 
     Wanted number of pairs
@@ -10,9 +12,16 @@ def generate_keypairs(num_pairs, logn=8):
     Outut:
     Randomly generated public/private key pairs for HAWK
     '''
+    
+    # TODO set an RNGcontext object from seed for testing
+
+    #rng = RngContext(seed)
+
     keypairs = [None]*num_pairs
     for p in range(num_pairs):
-        keypairs[p] = hawkkeygen(logn)
+        np.random.seed(seed+p)
+        rng = RngContext(np.random.randint(0, 256, 40, dtype=np.uint8))
+        keypairs[p] = hawkkeygen(logn, rng)
         print(f"{p+1}/{num_pairs} generated")
     return keypairs
 
@@ -20,5 +29,9 @@ def generate_F_G(f, g):
     return ntru_solve(f, g)
 
 if __name__ == "__main__":
-    keypairs = generate_keypairs(10)
-    print(keypairs)
+    keypairs = generate_keypairs(2, seed=13)
+    for i in range(len(keypairs)):
+
+        priv, pub = keypairs[i]
+        print(f"Private key: {priv}")
+        print(f"Public key:  {pub}")
