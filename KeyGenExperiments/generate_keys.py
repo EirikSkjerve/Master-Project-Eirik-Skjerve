c9a83b18d5e9bcc91eae9177afc14b5c6aaea54f
Eirik.Skjerve <eirik.skjerve@student.uib.no>
2024-04-18 10:43:54 +0200

    Removed the hawk_py folder from gitignore, since I will be doing changes there

--- KeyGenExperiments/generate_keys.py
+++ KeyGenExperiments/generate_keys.py
@@ -1,6 +1,6 @@
 # we import HAWK's python implementation of the hawk key generation algorithm
 from hawk_py.keygen import hawkkeygen
-
+from hawk_py.ntrugen.ntrugen_hawk import ntru_solve
 def generate_keypairs(num_pairs, logn=8):
     '''
     Input: 
@@ -13,11 +13,12 @@ def generate_keypairs(num_pairs, logn=8):
     keypairs = [None]*num_pairs
     for p in range(num_pairs):
         keypairs[p] = hawkkeygen(logn)
+        print(f"{p+1}/{num_pairs} generated")
     return keypairs
 
 def generate_F_G(f, g):
-    pass
+    return ntru_solve(f, g)
 
 if __name__ == "__main__":
-    keypairs = generate_keypairs(1)
-    print(keypairs[0])
+    keypairs = generate_keypairs(10)
+    print(keypairs)
