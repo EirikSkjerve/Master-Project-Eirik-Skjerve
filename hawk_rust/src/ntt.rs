237183901ca8965828b1d7a1eca726b681b3b248
Eirik Skjerve <tic013@uib.no>
2024-06-20 21:43:45 +0200

    Empty function for NTT representation of a polynomial

--- hawk_rust/src/ntt.rs
+++ hawk_rust/src/ntt.rs
@@ -1,2 +1,13 @@
 // This is the Number Theoretic Transform module
 // see https://eprint.iacr.org/2024/585.pdf
+
+
+// ntt of a polynomial
+pub fn ntt(f: Vec<i32>, p: u32) -> Vec<i32> {
+
+    let mut ntt_f = vec![0;f.len()];
+
+    // using *this* algorithm
+
+    return ntt_f;
+}
