ff7ea41af379b72f09e3dbe5dea37d4d3b77b42f
Eirik Skjerve <eirikds01@gmail.com>
2024-08-01 11:38:22 +0200

    Created empty helper functions

--- hawk_rust/src/ntt_fft.rs
+++ hawk_rust/src/ntt_fft.rs
@@ -13,8 +13,26 @@ pub fn ntt(f: Vec<i32>, p: u32) -> Vec<i32> {
 }
 
 // implementation of bit reversal of an integer
-pub fn brv(n: i32) -> i32 {
+fn brv(n: i32) -> i32 {
     // assert n power of 2
     // assert b = log2(n)
     return 0;
+}
+
+// given prime p and order n, compute 2n-th root of unity mod p
+fn get_roots(p: i32, n: i32) -> Vec<i32>{
+    let roots = vec![0; (n as usize)];
+    return roots;
+}
+
+// comptute zeta values in a specific order for usage in ntt/intt functions
+fn compute_zetas(root: i32, p: i32, n: i32) -> Vec<i32>{
+    let zetas = vec![0; (n as usize)];
+    return zetas;
+}
+
+// return the primitive root of prime p
+fn primitive_root(p: i32) -> i32 {
+    let g = 0;
+    return g;
 }
