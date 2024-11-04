b0d38bf9ad6f148cdb9e4845a01aca6ccb7dc920
Eirik Skjerve <eirikds01@gmail.com>
2024-11-04 13:21:27 +0100

    Add prefix when writing a vector to file

--- hawk_rust/src/write_to_file.rs
+++ hawk_rust/src/write_to_file.rs
@@ -3,10 +3,10 @@ use std::fs::File;
 use std::io::prelude::*;
 
 // write vector to file
-pub fn wvtf<T: Debug>(path: &str, v: &Vec<T>) -> std::io::Result<()> {
+pub fn wvtf<T: Debug>(path: &str, v: &Vec<T>, prefix: &str) -> std::io::Result<()> {
     let mut file = File::create(path)?;
     println!("{path}");
     println!("{:?}", v);
-    file.write_all(format!("{:?}", v).as_bytes())?;
+    file.write_all(format!("{}, {:?}", prefix, v).as_bytes())?;
     Ok(())
 }
