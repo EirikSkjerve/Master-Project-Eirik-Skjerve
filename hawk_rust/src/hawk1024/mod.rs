e929a9550d0169f8829c2a65d24bb5cee1ba619e
Eirik Skjerve <eirikds01@gmail.com>
2024-11-11 11:49:14 +0100

    Removed alot of stuff. Kept codec.rs for possible use later, but it needs rewriting

--- hawk_rust/src/hawk1024/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod codec_1024;
-pub mod hawkkeygen_1024;
-pub mod hawksign_1024;
-pub mod hawkverify_1024;
-pub mod verifyutils_1024;
--- hawk_rust/src/hawk512/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod codec_512;
-pub mod hawkkeygen_512;
-pub mod hawksign_512;
-pub mod hawkverify_512;
-pub mod verifyutils_512;
