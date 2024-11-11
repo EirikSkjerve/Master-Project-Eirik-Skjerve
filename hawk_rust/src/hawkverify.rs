1524151d273a9c65ab76bc92a18992b713b71726
Eirik Skjerve <eirikds01@gmail.com>
2024-11-11 10:13:54 +0100

    Hawkverify refactoring done

--- hawk_rust/src/hawkverify.rs
+++ hawk_rust/src/hawkverify.rs
@@ -13,7 +13,7 @@ pub fn ptc(a: &Vec<i64>, b: i64) -> Vec<i64> {
     a.clone().iter().map(|&x| x*b).collect()
 }
 
-fn hawkverify_inner(
+pub fn hawkverify(
     msg: &[u8], 
     pub_key: (&Vec<i64>, &Vec<i64>), 
     signature: &Vec<u8>, 
@@ -21,24 +21,33 @@ fn hawkverify_inner(
     n: usize
     ) -> bool {
 
+    //
+    // main method for verifying hawk signature on message
+    //
+
+    assert!(n==256||n==512||n==1024);
+
     // get the correct parameters
-    let (highs0, highs1, high00, high01) = match n {
+    let (highs0, highs1, high00, high01, sigmaverify) = match n {
         256 => ( hawk256_params::HIGHS0,
                  hawk256_params::HIGHS1,
                  hawk256_params::HIGH00,
-                 hawk256_params::HIGH01
+                 hawk256_params::HIGH01,
+                 hawk256_params::SIGMAVERIFY
             ),
         
         512 => ( hawk512_params::HIGHS0,
                  hawk512_params::HIGHS1,
                  hawk512_params::HIGH00,
-                 hawk512_params::HIGH01
+                 hawk512_params::HIGH01,
+                 hawk512_params::SIGMAVERIFY
             ),
 
         _ => ( hawk1024_params::HIGHS0,
                  hawk1024_params::HIGHS1,
                  hawk1024_params::HIGH00,
-                 hawk1024_params::HIGH01
+                 hawk1024_params::HIGH01,
+                 hawk1024_params::SIGMAVERIFY
             ),
     };
 
@@ -95,17 +104,18 @@ fn hawkverify_inner(
     let (p1, p2): (i64, i64) = (2147473409, 2147389441);
 
     let r1 = poly_qnorm(&q00, &q01, &w0, &w1, p1);
+    let r2 = poly_qnorm(&q00, &q01, &w0, &w1, p2);
 
-    false
-}
+    if r1 != r2 || modulo(r1, n as i64) != 0 {
+        println!("r1 != r2. Returning false");
+        return false;
+    }
 
-pub fn hawkverify(
-    msg: &[u8], 
-    pub_key: (&Vec<i64>, &Vec<i64>), 
-    signature: &Vec<u8>, 
-    n: usize
-) -> bool{
+    let r1 = r1/(n as i64);
+    
+    if (r1 as f64) > (8*n) as f64 * sigmaverify.powi(2) {
+        return false;
+    }
 
-    assert!(n==256||n==512||n==1024);
     false
 }
