2d4523bfd421971a2a1d7a215e408ec01a29340e
Eirik Skjerve <eirikds01@gmail.com>
2024-11-19 09:03:33 +0100

    Better comments for the gradient descent

--- hpp_rust/src/HPP/gradient_descent.rs
+++ hpp_rust/src/HPP/gradient_descent.rs
@@ -60,32 +60,47 @@ pub fn gradient_descent(
     let mut solutions: Vec<Matrix<i32, Const<1>, Dyn, VecStorage<i32, Const<1>, Dyn>>> =
         Vec::with_capacity(n);
 
+    // keep track of iterations
     let mut iterations = 0;
 
+    // initialize a new seed
     let mut rng_seed = rand::thread_rng();
     let mut seed: usize = rng_seed.gen();
 
     // for nice printouts
     let mut stdout = stdout();
 
+    // run loop until we have n unique solutions
     while solutions.len() < n {
-        let mut w = gen_u_vec(n, seed);
 
+        // create a random unit-vector
+        let mut w = gen_u_vec(n, seed);
         seed += 1;
+
+        // start descent for w
         loop {
             iterations += 1;
+
+            // compute approximation to gradient of w
             let g = nabla_mom4(&u, &w).transpose();
 
+            // let new w be w - delta*g, i.e. a new vector in the "correct" direction
             let mut w_new = w.clone() - (rate * g);
+            // normalize the vector so that it is on the unit sphere
             let norm_sq: f64 = w_new.iter().map(|&x| x.powi(2)).sum();
             w_new /= norm_sq.sqrt();
 
+            // if the fourth moment (approximation) is greater for the newly computed w,
+            // we have passed the global minima, and thus (a rounded) w is a correct solution
             if mom4(&u, &w) <= mom4(&u, &w_new) {
                 // round entries
                 let vtemp = w.clone().transpose() * linv.clone();
                 let v = vtemp.map(|x| x.round() as i32);
+                // we also check for -v
                 let neg_v = -v.clone();
 
+                // check if solution already exists
+                // if not, add the row as a solution
                 if !solutions.contains(&v) && !solutions.contains(&neg_v) {
                     solutions.push(v.clone());
 
@@ -94,17 +109,14 @@ pub fn gradient_descent(
                     print!("\r{}/{} vectors found!", solutions.len(), n);
 
                 }
+                // since we have found a row, we break the inner loop
                 break;
+                // otherwise, we update the current w to be the new w in the correct direction
             } else {
                 w = w_new;
             }
         }
     }
-
-    // println!(
-    //     "\nRUST HPP GRADIENT DESCENT COMPLETED IN {} ITERATIONS",
-    //     iterations
-    // );
 
     // convert vec of row vectors to matrix object
     let v_mat = Matrix::from_rows(&solutions);
